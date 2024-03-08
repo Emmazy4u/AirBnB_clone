d34f479bb6ecedbd09f3ea3c3cf006ac4c6fa73c
emma-obelenge <obeleemmanuel@gmail.com>
2024-03-08 03:28:00 +0000

    add first update on task 3 files

--- models/base_model.py
+++ models/base_model.py
@@ -12,34 +12,27 @@ class BaseModel:
 		self.id = str(uuid.uuid4())
 		self.created_at = datetime.utcnow()
 		self.update_at = datetime.utcnow()
-
-	def save(self):
-		"""
-		"""
-		self.updated_at = datetime.utcnow()
-	def to_dict(self):
-		"""
-		"""
-		inst_dict = self.__dict__.copy()
-		inst_dict["__class__"] = self.__class__.__name__
-		inst_dict["created_at"] = self.created_at.isoformat()
-		inst_dict["updated_at"] = self.updated_at.isoformat()
-		return inst_dict
+	
 	def __str__(self):
-		"""
-		"""
+		"""prints customized string formated output"""
 		class_name = self.__class__.__name__
 		return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
-	
-if __name__ == "__main__":
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-	print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+	def save(self):
+		"""would update the public instance attr 'updated_at'"""
+		self.updated_at = datetime.utcnow()
+
+	def to_dict(self):
+		"""
+		returns a dictionary containg keys/vaalues of __dict__
+		of the instance
+		"""
+		inst_dict_cpy = self.__dict__.copy()
+		inst_dict_cpy["__class__"] = self.__class__.__name__
+		inst_dict_cpy["created_at"] = self.created_at.isoformat()
+		inst_dict_cpy["updated_at"] = self.updated_at.isoformat()
+		return inst_dict_cpy
+
+	def to_json(self):
+		"""implement saving logic for console"""
+		pass
