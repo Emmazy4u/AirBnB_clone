e96a64fa6aa9ae89215a28c3277a7b0ce5115e4b
Amele06 <kelvina0306@gmail.com>
2024-03-09 10:29:02 -0800

    console updated

--- console.py
+++ console.py
@@ -1,138 +1,120 @@
 #!/usr/bin/python3
 """Entry point of the command interpreter"""
 import cmd
-import sys
 import shlex
-import re
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb)"
-
-    
-    def do_greet(self, line):
-        """function that prints hello"""
-        names = shlex.split(line)
-        for name in names:
-            print('hello', name)
-
+    prompt = "(hbnb) "
 
     def do_EOF(self, line):
-        """function that exits the console"""
+        """Exits the console"""
         print('exit')
         return True
-    
-    
+
     def do_quit(self, line):
-        """ command that quits the program"""
+        """Quits the console"""
         print('exit')
         return True
-    
-    
+
+    def default(self, line):
+        """default setting for cmd module if input is not valid"""
+        print(f"invalid command: {line}")
+        return False
+
     def emptyline(self):
-       """ returns nothing on ENTER"""
-       pass
-   
-   
+        """Does nothing on ENTER"""
+        pass
+
     def do_create(self, line):
-       """creates a new instance  of BaseModel and prints the id"""
-       if !line:
-           print("** class name missing **")
-       elif line not in storage.classes():
-           print("** class doesn't exist **")
-       else:
-           new_instance = storage.classes[line]()
-           new_instance.save()
-           print(new_instance.id)
+        """Creates a new instance of BaseModel and prints the id"""
+        if not line:
+            print("** class name missing **")
+        elif line not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            new_instance = storage.classes[line]()
+            new_instance.save()
+            print(new_instance.id)
 
-   
     def do_show(self, line):
-       """prints the string representation of an instance"""
-       if !line:
-           print('** class name missing **')
-       else:
-           className = line.split(' ')
-           if className[0] not in storage.classes():
-               print("** class doesn't exist **")
-           elif len(className) < 2:
-               print('** instance id missing **')
-           else:
-               key = "{}.{}".format(className[0], className[1])
-               if key not in storage.all():
-                   print('** no instance found **')
-               else:
-                   print(storage.all()[key])
+        """Prints the string representation of an instance"""
+        if not line:
+            print('** class name missing **')
+        else:
+            args = shlex.split(line)
+            if args[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print('** instance id missing **')
+            else:
+                key = "{}.{}".format(args[0], args[1])
+                instances = storage.all()
+                if key not in instances:
+                    print('** no instance found **')
+                else:
+                    print(instances[key])
 
-
-   def do_destroy(self, line):
-      """deletes an instance based on the class name and id"""
-      line = line.split(" ")
-      if !line:
-          print('** class name missing **')
-      else:
-          className = line[0]
-          if className not in storage.classes():
-              print("** class doesn't exist **")
-          elif len(className) < 2:
-              print('** instance id missing **')
-              key = f"{className[0]}.{className[1]}"
-              if key not in storage.all():
-                  print('** no instance found **')
-              else:
-                  del storage.all()[key]
-                  storage.save
-
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        args = shlex.split(line)
+        if not args:
+            print('** class name missing **')
+        elif args[0] not in storage.classes():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print('** instance id missing **')
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            instances = storage.all()
+            if key not in instances:
+                print('** no instance found **')
+            else:
+                del instances[key]
+                storage.save()
 
     def do_all(self, line):
-        """prints all string representation of all instances based or not on the class name"""
-        instance_obj = storage.all()
-        instance_list = []
-
-        if line != "":
-            for key, value in storage.all().items():
-                instance_list.append(str[value])
-            print(instance_list)
+        """Prints all string representations of all instances"""
+        if not line or line not in storage.classes():
+            print("** class doesn't exist **")
         else:
-            if line not in storage.classes():
-                print("** class doesn't exist **")
+            instances = storage.all()
+            f_instances = [str(value) for key, value in instances.items()
+                           if line in key]
+            print(f_instances)
 
-
-   def do_count(self, line):
-        """counts the instances of a class"""
+    def do_count(self, line):
+        """Counts the instances of a class"""
         counter = 0
         for key in storage.all().keys():
-            className ,instance_id = key.split(',')
-            if line == className:
+            class_name, instance_id = key.split('.')
+            if line == class_name:
                 counter += 1
-         print(counter)
+        print(counter)
 
-
-   def do_update(self, line):
-       """Updates an instance based on the class name and id by adding or updating attribute"""
-       if !line:
-           print('** class name missing **')
-       else:
-           className = line.split(" ")
-           if className[0] not in storage.classes():
-               print("** class doesn't exist **")
-               return
-           if len(className) < 2:
-               print('** instance id missing **')
-               return
-           key = f"{className[0]}.{className[1}}"
-           for key not in storage.all():
-               print('** no instance found **')
-               if len(className) < 3:
-                   print("** attribute name missing **")
-                   return
-               if len(className) < 4:
-                   print("** value missing **")
-                   return
-
-
-
-
-
+    def do_update(self, line):
+        """Updates an instance based on the class name and id"""
+        args = shlex.split(line)
+        if not args:
+            print('** class name missing **')
+        elif args[0] not in storage.classes():
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print('** instance id missing **')
+        else:
+            key = "{}.{}".format(args[0], args[1])
+            instances = storage.all()
+            if key not in instances:
+                print('** no instance found **')
+            elif len(args) < 4:
+                print("** attribute name missing **")
+            elif len(args) < 5:
+                print("** value missing **")
+            else:
+                instance = instances[key]
+                setattr(instance, args[2], eval(args[3]))
+                instance.save()
 
 
 if __name__ == '__main__':
