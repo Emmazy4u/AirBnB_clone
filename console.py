34583f0143c40096d5f303031cef079da08ed9a4
emma-obelenge <obeleemmanuel@gmail.com>
2024-03-10 01:15:57 -0800

    partial review of the console.py file

--- console.py
+++ console.py
@@ -2,29 +2,29 @@
 """Entry point of the command interpreter"""
 import cmd
 import shlex
-from models import storage
+#from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
-    def do_EOF(self, line):
-        """Exits the console"""
+    def do_quit(self, line):
+        """Quit command to exit the program"""
         print('exit')
         return True
 
-    def do_quit(self, line):
-        """Quits the console"""
-        print('exit')
-        return True
+    def do_EOF(self, line):
+     """Exits the console upon EOF condition"""
+     print('exit')
+     return True
 
     def default(self, line):
         """default setting for cmd module if input is not valid"""
-        print(f"invalid command: {line}")
+        print("invalid command: {}".format(line)
         return False
 
     def emptyline(self):
-        """Does nothing on ENTER"""
+        """Does nothing when space or ENTER is typed"""
         pass
 
     def do_create(self, line):
@@ -37,9 +37,9 @@ class HBNBCommand(cmd.Cmd):
             new_instance = storage.classes[line]()
             new_instance.save()
             print(new_instance.id)
-
+"""
     def do_show(self, line):
-        """Prints the string representation of an instance"""
+        ....Prints the string representation of an instance...
         if not line:
             print('** class name missing **')
         else:
@@ -57,7 +57,7 @@ class HBNBCommand(cmd.Cmd):
                     print(instances[key])
 
     def do_destroy(self, line):
-        """Deletes an instance based on the class name and id"""
+        ...Deletes an instance based on the class name and id...
         args = shlex.split(line)
         if not args:
             print('** class name missing **')
@@ -75,7 +75,7 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
 
     def do_all(self, line):
-        """Prints all string representations of all instances"""
+        ...Prints all string representations of all instances...
         if not line or line not in storage.classes():
             print("** class doesn't exist **")
         else:
@@ -85,7 +85,7 @@ class HBNBCommand(cmd.Cmd):
             print(f_instances)
 
     def do_count(self, line):
-        """Counts the instances of a class"""
+        ...Counts the instances of a class...
         counter = 0
         for key in storage.all().keys():
             class_name, instance_id = key.split('.')
@@ -94,7 +94,7 @@ class HBNBCommand(cmd.Cmd):
         print(counter)
 
     def do_update(self, line):
-        """Updates an instance based on the class name and id"""
+        ...Updates an instance based on the class name and id...
         args = shlex.split(line)
         if not args:
             print('** class name missing **')
@@ -115,7 +115,7 @@ class HBNBCommand(cmd.Cmd):
                 instance = instances[key]
                 setattr(instance, args[2], eval(args[3]))
                 instance.save()
-
+"""
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
