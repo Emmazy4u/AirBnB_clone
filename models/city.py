5c2b1484e99597bfdca8ec47d5bb6e60fadc1d5e
Amele06 <kelvina0306@gmail.com>
2024-03-11 12:27:23 -0700

    city updated

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """creates a city class that inherits from BaseModel"""
-from models.base_model import BaseMdel
+from models.base_model import BaseModel
 
 
 class City(BaseModel):
